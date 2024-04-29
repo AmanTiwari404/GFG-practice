bf6a78d8735f300fd92b686099b9139fd47b44d2
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-04-29 19:22:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove every kth node/remove-every-kth-node.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+	Node next;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+class Delete_Kth_Node
+{
+	Node head;
+	Node tail;
+	
+	void addToTheLast(Node node){
+		if(head == null){
+		head = node;
+		tail = node;
+		}
+		else
+		{
+		    tail.next = node;
+		    tail = tail.next ;
+		}
+	}	
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Delete_Kth_Node list = new Delete_Kth_Node();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            list.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				list.addToTheLast(new Node(a));
+			}
+			int k = sc.nextInt();					  
+			//System.out.println(list.head.data+" "+ k);
+			list.head = new Solution().delete(list.head,k); 
+			Node temp = list.head;
+			while(temp!=null)
+			{
+				System.out.print(temp.data+ " ");
+			    temp = temp.next;
+			}
+			System.out.println();
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Link list Node
+class Node
+{
+	Node next;
+	int data;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+class Solution
+{
+    /*You are required to complete this method*/
+    Node delete(Node head, int k)
+    {
+        // If k is 1, return null (no nodes will remain)
+        if (k == 1)
+            return null;
+
+        Node current = head;
+        Node previous = null;
+        int count = 0;
+
+        while (current != null) {
+            count++;
+            if (count % k == 0) { // Check if the current node is kth node
+                if (previous == null) { // If previous is null, update head to skip the kth node
+                    head = current.next;
+                } else {
+                    previous.next = current.next; // Skip the kth node
+                }
+            } else {
+                previous = current; // Update previous node
+            }
+            current = current.next; // Move to the next node
+        }
+
+        return head;
+    }
+}
