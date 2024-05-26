f2cb56e85c6110263ac66fce27a8a9d53de08e44
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-05-26 18:17:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Cost To Make Two Strings Identical/minimum-cost-to-make-two-strings-identical.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String X = sc.next();
+            String Y = sc.next();
+            int costX = sc.nextInt();
+            int costY = sc.nextInt();
+
+            Solution ob = new Solution();
+            System.out.println(ob.findMinCost(X, Y, costX, costY));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int findMinCost(String x, String y, int costX, int costY) {
+        int m = x.length();
+        int n = y.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Initialize base cases
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i * costX;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j * costY;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j] + costX, dp[i][j - 1] + costY);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
+    
