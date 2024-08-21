5b433883801126578953fff8b7b672ea35344399
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-08-21 23:34:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest path in Undirected Graph/shortest-path-in-undirected-graph.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);  
+        }
+        
+        int[] dist = new int[n];
+        Arrays.fill(dist, -1);  
+        dist[src] = 0;  
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(src);
+        
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            
+            for (int neighbor : adj.get(node)) {
+                if (dist[neighbor] == -1) { 
+                    dist[neighbor] = dist[node] + 1;
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        
+        return dist;
+    }
+}
