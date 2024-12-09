64a7460c0b997a6826659894a31eab33200d62d3
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-12-09 21:46:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert Interval/insert-interval.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[][] = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                a[i][0] = sc.nextInt();
+                a[i][1] = sc.nextInt();
+            }
+            int h[] = new int[2];
+            h[0] = sc.nextInt();
+            h[1] = sc.nextInt();
+            Solution obj = new Solution();
+            ArrayList<int[]> ans = obj.insertInterval(a, h);
+            System.out.print("[");
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.print("[");
+                System.out.print(ans.get(i)[0] + "," + ans.get(i)[1]);
+                System.out.print("]");
+                if (i != ans.size() - 1) System.out.print(",");
+            }
+            System.out.println("]");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> res = new ArrayList<>();
+        int i = 0, n = intervals.length;
+
+        while (i < n && intervals[i][1] < newInterval[0]) res.add(intervals[i++]);
+
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
+
+        while (i < n) res.add(intervals[i++]);
+
+        return res;
+    }
+}
