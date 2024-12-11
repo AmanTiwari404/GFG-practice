e305cfc5eb3b7e307b8205510400ce29ef9fdb2e
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-12-11 20:47:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+            String arr1[] = br.readLine().trim().split(" ");
+            String arr2[] = br.readLine().trim().split(" ");
+
+            int a[] = new int[arr1.length];
+            int b[] = new int[arr2.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(arr1[i]);
+            for (int i = 0; i < b.length; i++) b[i] = Integer.parseInt(arr2[i]);
+
+            Solution ob = new Solution();
+            ob.mergeArrays(a, b);
+
+            StringBuffer str = new StringBuffer();
+            for (int i = 0; i < a.length; i++) {
+                str.append(a[i] + " ");
+            }
+            System.out.println(str);
+            str = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                str.append(b[i] + " ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to merge the arrays.
+    public void mergeArrays(int a[], int b[]) {
+        int n = a.length, m = b.length, i = n - 1, j = 0;
+        while (i >= 0 && j < m && a[i] > b[j]) {
+            int t = a[i];
+            a[i] = b[j];
+            b[j] = t;
+            i--;
+            j++;
+        }
+        java.util.Arrays.sort(a);
+        java.util.Arrays.sort(b);
+    }
+}
