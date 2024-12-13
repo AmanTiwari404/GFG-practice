4c26460d82a5da60d0b78925045cfe9103da21e4
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-12-13 21:05:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        Solution ob = new Solution(); // Instantiate the Solution object once
+
+        while (t-- > 0) {
+            String line = scanner.nextLine();
+            String[] elements = line.split(" ");
+            int[] arr = new int[elements.length]; // Changed to int[]
+
+            for (int i = 0; i < elements.length; i++) {
+                arr[i] = Integer.parseInt(elements[i]);
+            }
+
+            System.out.println(ob.findMin(arr));
+            System.out.println("~");
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findMin(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] > arr[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return arr[l];
+    }
+}
