f9dea2c70f4cedbe6d7b0ce08fe1fba9fa5c5d69
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-12-31 16:23:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // Read first array
+            String input = br.readLine();
+            String[] inputArray = input.split(" ");
+            int[] arr = Arrays.stream(inputArray).mapToInt(Integer::parseInt).toArray();
+
+            Solution ob = new Solution();
+            int res = ob.longestConsecutive(arr);
+
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    // Function to return length of longest subsequence of consecutive integers.
+    public int longestConsecutive(int[] arr) {
+        if (arr.length == 0) return 0; 
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+
+       
+        for (int num : arr) {
+            
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
