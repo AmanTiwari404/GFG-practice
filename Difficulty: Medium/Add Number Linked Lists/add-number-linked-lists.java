0102197ecf9cc09c864f4601faf54090a9ec8d39
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2025-01-22 21:41:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+// driver
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+/* node for linked list
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    static Node addTwoLists(Node num1, Node num2) {
+        num1 = reverse(num1);
+        num2 = reverse(num2);
+        
+        Node result = null;
+        int carry = 0;
+        
+        while (num1 != null || num2 != null || carry > 0) {
+            int sum = carry;
+            
+            if (num1 != null) {
+                sum += num1.data;
+                num1 = num1.next;
+            }
+            
+            if (num2 != null) {
+                sum += num2.data;
+                num2 = num2.next;
+            }
+            
+            Node newNode = new Node(sum % 10);
+            newNode.next = result;
+            result = newNode;
+            
+            carry = sum / 10;
+        }
+        
+        while (result != null && result.data == 0 && result.next != null) {
+            result = result.next;
+        }
+        
+        return result;
+    }
+    
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        return prev;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GfG {
+
+    static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(read.readLine());
+        while (T-- > 0) {
+
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            Node num1 = new Node(Integer.parseInt(str[0]));
+            Node tail = num1;
+            for (int i = 1; i < n; i++) {
+                int val = Integer.parseInt(str[i]);
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+
+            String str2[] = read.readLine().trim().split(" ");
+            int m = str2.length;
+
+            Node num2 = new Node(Integer.parseInt(str2[0]));
+            tail = num2;
+            for (int i = 1; i < m; i++) {
+
+                tail.next = new Node(Integer.parseInt(str2[i]));
+                tail = tail.next;
+            }
+
+            Solution g = new Solution();
+            Node res = g.addTwoLists(num1, num2);
+            printList(res);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
