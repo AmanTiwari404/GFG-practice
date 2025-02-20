7e68bcd9887e20a5e75e133e7698cd0a172b09a4
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2025-02-20 11:41:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Find median in a stream/find-median-in-a-stream.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String[] parts = s.split(" ");
+            int[] nums = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                nums[i] = Integer.parseInt(parts[i]);
+            }
+            Solution ob = new Solution();
+            ArrayList<Double> ans = ob.getMedian(nums);
+            for (double i : ans) {
+                System.out.printf("%.1f ", i);
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public ArrayList<Double> getMedian(int[] arr) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // Left half
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // Right half
+        ArrayList<Double> result = new ArrayList<>();
+
+        for (int num : arr) {
+            if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+                maxHeap.add(num);
+            } else {
+                minHeap.add(num);
+            }
+
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.add(maxHeap.poll());
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.add(minHeap.poll());
+            }
+
+            if (maxHeap.size() == minHeap.size()) {
+                result.add((maxHeap.peek() + minHeap.peek()) / 2.0);
+            } else {
+                result.add((double) maxHeap.peek());
+            }
+        }
+        return result;
+    }
+
+}
