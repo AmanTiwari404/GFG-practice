dac106f559f31a02fc672d32653410c1049a6298
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2025-02-20 11:37:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test_cases = Integer.parseInt(br.readLine().trim());
+
+        while (test_cases-- > 0) {
+            // Read the array from input line
+            String[] input = br.readLine().trim().split(" ");
+            int[] arr = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                arr[i] = Integer.parseInt(input[i]);
+            }
+
+            // Solution instance to invoke the function
+            Solution ob = new Solution();
+            int result = ob.hIndex(arr);
+
+            System.out.println(result);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);  
+        
+        int n = citations.length;
+        int hIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i; 
+            if (citations[i] >= h) {
+                hIndex = h;
+                break;
+            }
+        }
+        return hIndex;
+    }
+}
