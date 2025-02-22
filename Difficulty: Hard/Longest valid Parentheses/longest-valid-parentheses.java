fc3b93632c62b07a13f5b80feeac9bdb7a71088a
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2025-02-22 15:10:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
@@ -1,51 +1,52 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
+        while (t-- > 0) {
             String S = in.readLine();
-            
+
             Solution ob = new Solution();
             System.out.println(ob.maxLength(S));
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
 
-class Solution{
-    static int maxLength(String S){
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
+class Solution {
+    static int maxLength(String s) {
+        int maxLen = 0;
+        int left = 0, right = 0;
+
         
-        int maxLength = 0; 
-        
-        for (int i = 0; i < S.length(); i++) {
-            char ch = S.charAt(i);
-            
-            if (ch == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-            
-                if (!stack.isEmpty()) {
-                    int length = i - stack.peek();
-                    maxLength = Math.max(maxLength, length);
-                } else {
-                    stack.push(i);
-                }
-            }
+        for (char c : s.toCharArray()) {
+            if (c == '(') left++;
+            else right++;
+
+            if (left == right) maxLen = Math.max(maxLen, 2 * right);
+            else if (right > left) left = right = 0; // Reset when invalid
         }
+
+        left = right = 0;
+
         
-        return maxLength;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') left++;
+            else right++;
+
+            if (left == right) maxLen = Math.max(maxLen, 2 * left);
+            else if (left > right) left = right = 0; // Reset when invalid
+        }
+
+        return maxLen;
     }
 }
