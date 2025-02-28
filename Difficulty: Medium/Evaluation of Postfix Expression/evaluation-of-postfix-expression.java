4bf3a405d86a2b0b16b2507fbd14fda56bce41bd
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2025-02-28 21:19:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Evaluation of Postfix Expression/evaluation-of-postfix-expression.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int evaluate(String[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String token : arr) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(applyOperation(a, b, token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        return stack.pop();
+    }
+    
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+    
+    private int applyOperation(int a, int b, String operator) {
+        switch (operator) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b; 
+            default: throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        int t = Integer.parseInt(line);
+        while (t-- > 0) {
+            line = reader.readLine();
+            String[] arr = line.split(" ");
+            Solution solution = new Solution();
+            System.out.println(solution.evaluate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
