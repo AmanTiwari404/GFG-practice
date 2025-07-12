3b5ccedf79c6b710fc28d5c481172bccdd370cf8
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2025-07-12 21:51:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        int n = arr.length;
+        for(int i = 0; i < n/2; i++){
+            int temp = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+        
+    }
+}
